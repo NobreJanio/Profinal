ca41a3c534802ad6170b9095986ddda25a3e24b6
NobreJanio <janionobrega@hotmail.com>
2021-11-27 01:22:02 -0300

    criado o css
    *, main, body, header e .card

---
+++ js/script1.js
@@ -0,0 +1,20 @@
+class Cursos {
+
+    constructor() {
+        this.id = 0;
+        this.nomeCurso = '';
+        this.nomeProfessor  = '';
+    }
+    
+    adicionar() {
+        
+        alert('Vamos adicionar um produto');
+    
+    }
+
+    excluir() {
+        alert('Item deletado')
+    }
+}
+
+var cursos = new Cursos();
